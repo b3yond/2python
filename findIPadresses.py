ce0b7f1b4e1f282a2c2083278d0f6fee32603d31
b3yond <b3yond@riseup.net>
2016-08-10 20:05:26 +0200

    autodownload dependencies

--- findIPadresses.py
+++ findIPadresses.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 __author__ = 'medusa'
 
-import pygeoip
+import os
+try:
+    import pygeoip
+except ImportError:
+    print "[ERROR] No pygeoip installed"
+    print "Installing pygeoip..."
+    os.system("sudo pip install pygeoip")
+    import pygeoip
 
+
+home = os.environ['HOME']
+if not os.path.isfile(home + '/files/GeoLiteCity.dat'):
+    print "[ERROR] GeoLiteCity database missing"
+    print "Downloading database to " + home + "/files/"
+    os.system("cd " + home + " && wget -N -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz && gzip -d GeoLiteCity.dat.gz")
 print "Thanks to OccupyTheWeb, full tutorial here:  http://null-byte.wonderhowto.com/how-to/hack-like-pro-find-exact-location-any-ip-address-0161964/"
-gip = pygeoip.GeoIP('/home/b3yond/files/GeoLiteCity.dat')
+gip = pygeoip.GeoIP(home + '/files/GeoLiteCity.dat')
 
-ip = raw_input("What IP-Adress do you want to trace? ")
-
-rec = gip.record_by_addr(ip)
+ip = 0
+while ip == 0:
+    ip = raw_input("What IP-Adress do you want to trace? ")
+    try:
+        rec = gip.record_by_addr(ip)
+    except socket.error:
+        print "[ERROR] Illegal IP address. Try again."
+        ip = 0
 lst = rec.items()
 for key,val in lst:
     print "%s: %s" %(key,val)
